d08391e5e64bd8af023f8a2bf05f8720ff4613c2
Adam Dasovich <adamdasovich@gmail.com>
2025-02-21 08:41:06 -0500

    UserProfile

--- src/components/user/UserProfilePage.jsx
+++ src/components/user/UserProfilePage.jsx
@@ -1,7 +1,27 @@
 import UserInfo from './UserInfo'
 import OrderHistoryContainer from './OrderHistoryContainer'
+import { useState, useEffect } from 'react'
+import api from '../../api'
 
 const UserProfilePage = () => {
+
+  const [userInfo, setUserInfo] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    api.get('user_info')
+    .then(res => {
+      console.log(res.data)
+      setUserInfo(res.data)
+      setLoading(false)
+    })
+    .catch(err => {
+      console.log(err.message)
+      setLoading(false)
+    })
+  }, [])
+
   return (
     <div className='container my-5'>
 
