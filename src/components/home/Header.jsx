c1843e1843f1bc99ae0da3f6951ecc2cf76eebd4
Adam Dasovich <adamdasovich@gmail.com>
2025-07-30 09:21:14 -0400

    all working

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
         <div className='container px-4 px-lg-5 my-5'>
             <div className='text-center text-white'>
                 <h1 className='display-4 fw-bold' style={{color: '#AA336A'}}>Welcome</h1>
-                <p className='lead fw-normal text-white-75 mb-4'>Discover some stuff</p>
-                <a href='#shop' className='btn btn-light btn-lg rounded-pill px-4 py-2' >Shop Now</a>
+                <p className='lead fw-normal text-white-75 mb-4'>Discover some fucked up stuff</p>
+                <a href='#shop' className='btn btn-light btn-lg rounded-pill px-4 py-2' >Get your wallet out tight wad</a>
             </div>
         </div>
     </header>
